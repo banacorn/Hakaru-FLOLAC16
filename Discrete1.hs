c51404755d46d75909961a95f411048c3dca3234
Tîng-Giān Luā <banacorn@gmail.com>
2016-07-13 09:53:50 +0800

    sync with the ftp

--- Discrete1.hs
+++ Discrete1.hs
@@ -5,9 +5,9 @@
 
 module Discrete where
 
+import Control.Applicative
 import qualified Data.Map as M
 import System.Random
-import Control.Monad (ap)
 
 -- ------------------------------------------------------------------------
 -- Representing distributions
@@ -18,6 +18,7 @@ import Control.Monad (ap)
 
 xor :: Bool -> Bool -> Bool
 xor x y = not (x == y)
+
 
 -- We need to represent graphical models like Grass and the two-coin model
 -- Draw the model.
@@ -30,6 +31,7 @@ xor x y = not (x == y)
 -- Pr(Coin1=x) * Pr(Coin2=y|Coin1=x) * Pr((x = not y)=t|Coin1=x,Coin2=y)
 --  {conditional independence, from the graph}
 -- Pr(Coin1=x) * Pr(Coin2=y) * Pr((x = not y)=t|Coin1=x,Coin2=y)
+
 
 
 -- Let's define operations we need
@@ -63,6 +65,7 @@ twocoins' = chain' (bern' 0.5) $ \x ->
             dirac' (xor x y)
 -- what is the type?
 -- How is the conditional independence expressed?
+-- (lexically, dependence on x and y)
 
 -- How to compute the probability if in two successive tosses,
 -- there is one head, one tail?
@@ -99,11 +102,16 @@ twocoins'' = chain'' (bern'' 0.5) $ \x ->
 
 -- We haven't lost anything
 
-twocoins1'' = undefined
+twocoins1'' = 
+  chain'' (bern'' 0.5) $ \x ->
+  chain'' (bern'' 0.5) $ \y ->
+  dirac'' (x,(y,(xor x y)))
 
 -- The notation is a bit ungainly though...
 
--- What a happy coincidence!
+-- What a happy coincidence! Haskell has the right good notation. We only
+-- need to rename our conditional and dirac into somewhat historical names
+-- (>>=) and return.
 
 class Monad d => Dist d where
   bern :: Prob -> d Bool
@@ -132,9 +140,11 @@ twocoins1 = do
 class Dist d => Die d where
   die :: d Int   -- in Agda, Finite 6. But we do Int for now
 
--- dieroll n = undefined
+dieroll n = fmap sum $ sequence $ replicate n die
 
--- _ = runExact (dieroll 3)
+_ = runExact (dieroll 3)
+
+-- What are those functions, fmap and sequence?
 
 -- Implementing grass model
 
@@ -162,7 +172,11 @@ _ = runExact grass_fwd
 -- Point: recursive implementation
 
 uniformly :: Ord a => [a] -> Dst a
-uniformly = undefined
+uniformly []  = failure
+uniformly [a] = return a
+uniformly lst@(h:t) = do
+  f <- bern (1/fromIntegral (length lst))
+  if f then return h else uniformly t
 
 _ = runExact $ uniformly ([]::[Int])
 _ = runExact $ uniformly [1]
@@ -206,13 +220,105 @@ https://en.wikipedia.org/wiki/Monty_Hall_problem
 -}
 
 monty_hall :: Bool -> Dst Bool
-monty_hall = undefined
+monty_hall should_switch = do
+  car_door    <- uniformly doors
+  my_choice   <- uniformly doors
+  opened_door <- uniformly (del car_door . del my_choice $ doors)
+  if should_switch then
+    let [remaining_door] = del opened_door . del my_choice $ doors in
+    return $ remaining_door == car_door
+  else
+    return $ my_choice == car_door
+ where
+   doors = [1,2,3]
+   -- Delete an element from a list of was present
+   del :: Eq a => a -> [a] -> [a]
+   del x [] = []
+   del x (h:t)
+     | x == h     = t
+     | otherwise  = h : del x t
 
 _ = runExact $ monty_hall False
 _ = runExact $ monty_hall True
 
 -- ------------------------------------------------------------------------
 -- Conditioning
+
+-- Let's go back to the running example (slightly more complicated):
+-- We flip a fair coin thrice. We now *observed* that the
+-- results of three flips are not all the same.
+-- What is the (posterior) probability that the first coin came up head (True)?
+-- (draw the model)
+
+{-
+ We want to compute
+ Pr(Coin1=x|TheSame=False)
+    { Multiply/divide by Pr(TheSame=False) }
+ = Pr(Coin1=x|TheSame=False) * Pr(TheSame=False) / Pr(TheSame=False)
+    {Chain rule; obtaining the Joint distribution}
+ = Pr(Coin1=x,TheSame=False) / Pr(TheSame=False)
+    {Integrating out}
+ = Sum_y Sum_z Pr(Coin1=x,Coin2=y,Coin3=z,TheSame=False) /
+   Sum_x Sum_y Sum_z Pr(Coin1=x,Coin2=y,Coin3=z,TheSame=False)
+
+How to find the above numerator and denominator from the joint distribution:
+  False False False  True     1/8
+  False False True   False    1/8
+  False True False   False    1/8
+  False True  True   False    1/8
+  True  False False  False    1/8
+  True  False True   False    1/8
+  True  True  False  False    1/8
+  True  True  True   True     1/8
+
+First, remove all rows where TheSame is not False
+
+  False False True   False    1/8
+  False True False   False    1/8
+  False True  True   False    1/8
+  True  False False  False    1/8
+  True  False True   False    1/8
+  True  True  False  False    1/8
+
+Then integrate out Coin2 and Coin3 (do Sum_y and Sum_z)
+
+  False    False    3/8
+  True     False    3/8
+
+for the last factor, we just sum up: 6/8
+The desired result
+
+  False    False    1/2
+  True     False    1/2
+
+-}
+
+-- How to program this
+-- Recall, the joint distribution corresponds to a model
+
+joint3:: Dst (Bool,Bool,Bool,Bool)
+joint3 = do
+  coin1 <- bern 0.5
+  coin2 <- bern 0.5
+  coin3 <- bern 0.5
+  same  <- return (coin1 == coin2 && coin2 == coin3)
+  return (coin1,coin2,coin3,same)
+
+_ = runExact joint3
+
+-- Pr(Coin1=x|Same=Fasle)
+one_not_same = do
+  (coin1,coin2,coin3,same) <- joint3
+  if same then failure  -- erasing rows
+     else return coin1  -- coin2 and coin3 are integrated out
+
+_ = runExact one_not_same
+
+-- But we didn't compute the term in the denominator.
+-- How to do that?
+
+_ = normalize $ runExact one_not_same
+
 
 normalize :: PT a -> PT a
 normalize pt = map (map_snd (/nf)) pt
@@ -226,6 +332,20 @@ girl = undefined
 
 -- _ = runExact girl
 -- _ = normalize $ runExact girl
+
+-- Grass model
+
+grass_wet = do
+  rain         <- bern 0.3
+  sprinkler    <- bern 0.5
+  grass_is_wet <- nor 0.9 0.8 0.1 rain sprinkler
+  undefined
+
+-- Pr(Rain|Wet)
+-- The result matches what we had before. Much simpler this time, right?
+
+
+
 
 -- Fair coin tosses given arbitrarily biased coin (von Neumann trick)
 -- Show/Prove the correctness
@@ -247,13 +367,79 @@ drunk_coin :: Dst Bool
 drunk_coin = undefined
 
 -- Compute AND of n tosses of the drunk coin 
--- dcoin_and n = 
+dcoin_and n = undefined
 
 -- _ = runExact $ dcoin_and 10
 -- [(False,9.990234374999978e-11),(True,9.765624999999978e-14)]
 
 -- Can we implement more efficiently?
 -- (shorthand and) Why the False probability changes?
+
+-- Alarm puzzle
+-- http://aima.eecs.berkeley.edu/slides-pdf/chapter14a.pdf
+-- p5 and 6
+-- See examples on pp19 and 20
+
+
+-- HMM
+{-
+  States represent position of a 1-dimensional random walk on 1..8
+
+  State transition: one position to left/right with prob. 0.3 each;
+  stay in state with prob. 0.4 (0.7 for boundary states)
+
+  Observable: two states 'l' and 'r'. Prob. of observation=l in state
+  k is 1-(k-1)/7
+-}
+
+newtype HMMState = HS{unHS:: Int} deriving (Show,Eq,Ord)
+instance Bounded HMMState where
+  minBound = HS 1
+  maxBound = HS 8
+
+instance Enum HMMState where
+  succ (HS i) = HS (i+1)
+  pred (HS i) = HS (i-1)
+  
+-- Transition Probabilities
+transitions :: HMMState -> PT HMMState
+transitions st = case () of
+  _ | st == minBound -> [(st,0.7),(succ st,0.3)]
+  _ | st == maxBound -> [(st,0.7),(pred st,0.3)]
+  _                  -> [(st,0.4),(succ st,0.3),(pred st,0.3)]
+
+-- Observations
+data HMMObs = L | R
+-- Tabulate the observation probabilities: of observing L
+-- We use the numeric values directly from the Primula code
+l_observation_prob :: M.Map HMMState Prob
+l_observation_prob = M.fromList $
+   zip [minBound..maxBound] 
+       [1.0, 0.85714, 0.71428, 0.57142, 0.42857, 0.28571, 0.14285, 0.0]
+
+-- The evolution function: compute the state for the next step
+evolve :: HMMState -> Dst HMMState
+evolve = categorical . transitions
+
+hmmobserve :: HMMState -> Dst HMMObs
+hmmobserve st =
+  case (M.!) l_observation_prob st of
+    0 -> return R
+    1 -> return L
+    p -> categorical [(L,p), (R,1-p)]
+
+-- Queries
+-- Run the model for N steps, asserting observations
+
+{-
+do_evolve n evidence =
+  let st0 = uniformly [minBound..maxBound] in
+  let rec iter i n st =
+    if i > n then st
+    else let () = evidence st i in		(* check the evidence *)
+         iter (succ i) n (evolve st)
+  in iter 1 n st0
+-}
 
 
 -- ------------------------------------------------------------------------
@@ -284,9 +470,9 @@ instance Functor Dst where
    fmap f (Chain d k) = Chain d (fmap f . k)
 
 instance Applicative Dst where
-   pure = return
-   (<*>) = ap 
-  
+  pure    = return
+  m <*> x = do {mv <- m; xv <- x; return (mv xv)}
+
 instance Monad Dst where
   return x = Single [(x,1)]
 
@@ -296,6 +482,10 @@ instance Monad Dst where
   Single pt      >>= k = Chain pt k
   
   -- Exercise: justify this
+  -- Sum_y (Sum_x Pr(X=x) * Pr(Y=y|X=x)) * Pr(Z=z|Y=y)
+  -- Sum_y (Sum_x Pr(X=x) * Pr(Y=y|X=x) * Pr(Z=z|Y=y))
+  -- Sum_x (Sum_y Pr(X=x) * Pr(Y=y|X=x) * Pr(Z=z|Y=y))
+  -- Sum_x Pr(X=x) * (Sum_y Pr(Y=y|X=x) * Pr(Z=z|Y=y))
   Chain pt k1 >>= k = Chain pt (\x -> k1 x >>= k)
 
 
@@ -339,4 +529,98 @@ runSample n m = go (mkStdGen 17) n m
 _ = runSample 10 twocoins
 
 _ = let n = 20 in (/ fromIntegral n) . fromIntegral . length . filter id $ runSample n grass_fwd
+
+categorical :: PT a -> Dst a
+categorical = Single
+
+
+-- ------------------------------------------------------------------------
+-- Grass Model
+
+-- Probability the grass is wet, computed by hand
+-- It is easier to compute the probability the grass is dry.
+-- All three inputs (including noise) to nor should be false.
+-- They are independent, so we just multiply the probability.
+-- The input from rain is false if either there is no rain,
+-- or there was rain, but it did not wet the grass.
+grass_wet_hand = 1 - grass_dry
+ where
+   grass_dry = 0.9 * (0.7 + 0.3*0.1) * (0.5 + 0.5*0.2)
+
+-- Computing the (posterior) probability of rain having observed
+-- the grass is wet.
+-- Bayes formula:
+-- Pr(G,R) = Pr(R|G) * Pr(G) = Pr(G|R) * Pr(R)
+-- Thus Pr(R|G) = Pr(G|R) * Pr(R) / Pr(G)
+-- We have computed Pr(G) earlier.
+rain_if_grass_wet_hand = grass_wet_rain * rain / grass_wet_hand
+ where
+   rain = 0.3
+   grass_wet_rain = 1 - grass_dry_rain
+   grass_dry_rain = 0.9 * 0.1 * (0.5 + 0.5*0.2)
+-- 0.4684714427203697
+
+spr_if_grass_wet_hand = grass_wet_spr * spr / grass_wet_hand
+ where
+   spr = 0.5
+   grass_wet_spr = 1 - grass_dry_spr
+   grass_dry_spr = 0.9 * (0.7 + 0.3*0.1) * 0.2
+-- 0.7169032684054144
+
+-- Hence, when we see wet grass, it is quite more likely this is because the
+-- sprinkler was on.
+
+-- ------------------------------------------------------------------------
+-- Representing distributions, one approach
+
+-- running example: flipping a fair coin twice and obtaining
+-- the probability the two outcomes are different
+
+-- Draw the graphical model
+
+repr1 =
+  let x = bern 0.5 in
+  let y = bern 0.5 in
+  x `xor` y
+ where bern = undefined  -- for now
+
+-- What is wrong with this picture?
+-- Consider substituting Equals for Equals:
+
+repr1' =
+  let x = bern 0.5 in
+  x `xor` x  -- which is always false
+ where bern = undefined  -- for now
+
+-- We have to distinguish between the distribution (bern 0.5) and
+-- a sample drawn from that distribution.
+-- Second attempt
+
+repr2 = sampleAndThen (bern 0.5) $ \x ->
+        sampleAndThen (bern 0.5) $ \y ->
+   x `xor` y
+   where
+     -- Dist a -> (a -> w) -> w
+     sampleAndThen = undefined
+     -- Dist Bool
+     bern = undefined
+
+-- Is the type of sampleAndThen OK?
+-- What prevents us from writing
+
+repr2' =
+  let x = sampleAndThen (bern 0.5) id in
+  let y = sampleAndThen (bern 0.5) id in
+  x `xor` y
+   where
+     -- Dist a -> (a -> w) -> w
+     sampleAndThen = undefined
+     -- Dist Bool
+     bern = undefined
+
+-- and then we are back where we started.
+
+-- We need something better
+-- We need
+--  sampleAndThen to have the type Dist a -> (a -> Dist b) -> Dist b
 
